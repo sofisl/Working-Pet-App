3fba60a45f52d5a8625bab3bdd32f5001a613524
Sofia Leon <sofialeon@sofialeon-macbookpro.roam.corp.google.com>
2019-10-11 14:11:09 -0700

    experimentation...it works...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,14 @@ import {UserService} from "./services/userService";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private readonly dogAuthenticate: DogAuthenticate, private readonly userService: UserService) { }
+  constructor(public readonly dogAuthenticate: DogAuthenticate, public readonly userService: UserService) { }
   isUAdmin = false;
   userState = null;
 
   async ngOnInit() {
     console.log(this.dogAuthenticate.watch()); //undefined
     console.log(this.dogAuthenticate.state); //null
-    // console.log(await this.isUserAdmin());
-    // console.log(await this.dogState());
+
   }
 
   async signIn() {
@@ -29,25 +28,25 @@ export class AppComponent {
     
   }
 
-  async dogState() {
-    this.dogAuthenticate.watch();
-    if (this.dogAuthenticate.watch() != null) {
-      this.userState = true;
-    } else {
-      this.userState = false;
-    }
-    return this.userState;
-  }
+  // async dogState() {
+  //   this.dogAuthenticate.watch();
+  //   if (this.dogAuthenticate.watch() != null) {
+  //     this.userState = true;
+  //   } else {
+  //     this.userState = false;
+  //   }
+  //   return this.userState;
+  // }
 
-  async isUserAdmin() {
-    await this.userService.getUserAdminStatus();
-    if (this.userService.getUserAdminStatus()) {
-      this.isUAdmin = true;
-    } else {
-      this.isUAdmin = false;
-    }
-    return this.isUAdmin;
-  }
+  // async isUserAdmin() {
+  //   await this.userService.getUserAdminStatus();
+  //   if (this.userService.getUserAdminStatus()) {
+  //     this.isUAdmin = true;
+  //   } else {
+  //     this.isUAdmin = false;
+  //   }
+  //   return this.isUAdmin;
+  // }
 
   
 }
